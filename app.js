9ba3db2711be4ff312ac9b126f673af269bb5d57
estebanrfp <estebanrfp@gmail.com>
2015-06-03 17:06:37 +0200

    Cambiamos la estructura inicial

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
 var ical = require('ical-generator'),
+    http = require('http'),
+    cal = ical({domain: 'github.com', name: 'my first iCal'});
  
-    // Create new Calendar and set optional fields 
-    cal = ical({
-        domain: 'desarrolloactivo.com',
-        prodId: {company: 'platzicursos-desarrolloactivo.com', product: 'ical-generator'},
-        name: 'My Testfeed',
-        timezone: 'Europe/Berlin'
-    });
+// overwrite domain 
+cal.domain('sebbo.net');
  
-// You can also set values like this… 
-cal.domain('desarrolloactivo.com');
- 
-// … or get values 
-cal.domain(); // --> "sebbo.net" 
- 
-// create a new event 
-var event = cal.createEvent({
+cal.createEvent({
     start: new Date(),
     end: new Date(new Date().getTime() + 3600000),
-    timestamp: new Date(),
-    summary: 'My Event',
-    organizer: 'Esteban Fuster <estebanrfp@gmail.com>'
+    summary: 'Example Event',
+    description: 'It works ;)',
+    location: 'my room',
+    url: 'http://sebbo.net/'
 });
  
-// like above, you can also set/change values like this… 
-event.summary('My Super Mega Awesome Event');
- 
-// get the iCal string 
-cal.toString(); // --> "BEGIN:VCALENDAR…" 
- 
- 
-// You can also create events directly with ical() 
-cal = ical({
-    domain: 'desarrolloactivo.com',
-    prodId: '//superman-industries.com//ical-generator//EN',
-    events: [
-        {
-            start: new Date(),
-            end: new Date(new Date().getTime() + 3600000),
-            timestamp: new Date(),
-            summary: 'My Event',
-            organizer: 'Esteban Fuster <estebanrfp@gmail.com>'
-        }
-    ]
-}).toString();
+http.createServer(function(req, res) {
+    cal.serve(res);
+}).listen(3000, '127.0.0.1', function() {
+    console.log('Server running at http://127.0.0.1:3000/');
+});
