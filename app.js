08ae9bc9aa7d06eff58a0c9572e8eb79c0805df7
estebanrfp <estebanrfp@gmail.com>
2015-06-03 17:03:02 +0200

    nuevos cambios en app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var ical = require('ical-generator'),
  
     // Create new Calendar and set optional fields 
     cal = ical({
-        domain: 'sebbo.net',
-        prodId: {company: 'superman-industries.com', product: 'ical-generator'},
+        domain: 'desarrolloactivo.com',
+        prodId: {company: 'platzicursos-desarrolloactivo.com', product: 'ical-generator'},
         name: 'My Testfeed',
         timezone: 'Europe/Berlin'
     });
  
 // You can also set values like this… 
-cal.domain('sebbo.net');
+cal.domain('desarrolloactivo.com');
  
 // … or get values 
 cal.domain(); // --> "sebbo.net" 
@@ -20,7 +20,7 @@ var event = cal.createEvent({
     end: new Date(new Date().getTime() + 3600000),
     timestamp: new Date(),
     summary: 'My Event',
-    organizer: 'Sebastian Pekarek <mail@example.com>'
+    organizer: 'Esteban Fuster <estebanrfp@gmail.com>'
 });
  
 // like above, you can also set/change values like this… 
@@ -32,7 +32,7 @@ cal.toString(); // --> "BEGIN:VCALENDAR…"
  
 // You can also create events directly with ical() 
 cal = ical({
-    domain: 'sebbo.net',
+    domain: 'desarrolloactivo.com',
     prodId: '//superman-industries.com//ical-generator//EN',
     events: [
         {
@@ -40,7 +40,7 @@ cal = ical({
             end: new Date(new Date().getTime() + 3600000),
             timestamp: new Date(),
             summary: 'My Event',
-            organizer: 'Sebastian Pekarek <mail@example.com>'
+            organizer: 'Esteban Fuster <estebanrfp@gmail.com>'
         }
     ]
 }).toString();
